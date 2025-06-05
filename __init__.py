bc3648de1a1b06e78130d13681d1db0405dbf225
Regan <sooboy@foxmail.com>
2025-06-05 12:23:20 +0800

    inti

---
+++ __init__.py
@@ -0,0 +1,7 @@
+"""DTraderHQ Python WebSocket 客户端"""
+
+from .client import DTraderHQClient, MarketData
+
+__version__ = "1.0.0"
+__author__ = "DTraderHQ"
+__all__ = ["DTraderHQClient", "MarketData"]
